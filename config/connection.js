7051e882f186e13a2635cf0d3e8b02bd3eec851d
HB <hbianan@gmail.com>
2019-08-17 09:03:18 -0400

    file updated

--- config/connection.js
+++ config/connection.js
@@ -19,5 +19,15 @@ if(process.env.JAWSDB_URL){
   });
 }
 
+//connecting...
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
   //exporting the connection from this file
   module.exports = connection;
